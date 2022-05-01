be1f90050c19b6f17ee21e3c2632bb0ddbab77aa
DamirNabiull <51910382+DamirNabiull@users.noreply.github.com>
2022-05-01 20:34:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import Music
 import GA
 
 
-file = 'input3.mid'
+file = 'input2.mid'
 
 MidiGen = Music.MidiGenerator(file)
 
@@ -19,18 +19,22 @@ ChordGen = Notes.ChordGenerator(tonic, mode)
 
 ga = GA.GeneticAlgorithm(ChordGen, MidiGen.chords_count, tonic, mode, ticks, min_note, partition)
 
-epochs = 1
-iterations = 10
-initial_pop = 10
+epochs = 25
+iterations = 200
+initial_pop = 10  # >= 1
 mutation_prob = 0.8
 best = []
 sl = []
 
 for i in range(epochs):
-    print(i)
-    sorted_c, sl = ga.run_ga(initial_pop, mutation_prob, iterations, best)
-    best.append(sorted_c[-1])
+    print('Epoch:', i)
+    sorted_c = []
+    if i == 0:
+        sorted_c, sl = ga.run_ga(initial_pop, mutation_prob, iterations, best)
+    else:
+        sorted_c, sl = ga.run_ga(initial_pop//2, mutation_prob, iterations, best)
 
-print(sl)
+    best = sorted_c[-initial_pop//2:]
+
 chords = best[-1].get_genes()
-MidiGen.create_new_track(chords)
+MidiGen.create_new_track(chords, file[:-4] + '_output')
